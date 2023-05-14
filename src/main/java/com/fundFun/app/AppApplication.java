464a42765a70177541458a9f5dac39aae69c706d
leewooyup <zjadlspun1114@naver.com>
2023-05-14 13:43:30 +0900

    세팅

---
+++ src/main/java/com/fundFun/app/AppApplication.java
@@ -0,0 +1,13 @@
+package com.fundFun.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
